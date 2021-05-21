cef8db59bb8f6e6abcdec01fda725275a35883c5
hrithik <hrithik@PINGHERE.whitefoxx>
2021-05-21 00:49:51 -0400

    test

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div className="flex bg-amazon_blue w-1128 h-800 text-white ">
+    <div className=" hidden flex bg-amazon_blue w-1128 h-800 text-white ">
       <div className="ml-40  text-center relative flex flex-col m-2  z-30 p-10 cursor-pointer">
         <p className="font-extrabold"> Get to Know Us</p>
         <br />
