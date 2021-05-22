1e1245f71a1263f96da3b4aedc3519264118c1fc
hrithik <hrithik@PINGHERE.whitefoxx>
2021-05-22 08:31:57 -0400

    checkout

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZyXfF28RcbZqpFSgjHSCld2iSwIp4zfA",
+  authDomain: "clone-fully-responsive.firebaseapp.com",
+  projectId: "clone-fully-responsive",
+  storageBucket: "clone-fully-responsive.appspot.com",
+  messagingSenderId: "748443151693",
+  appId: "1:748443151693:web:3266b49960ba9eb3f15b04",
+  measurementId: "G-CTWNQ02KJ9",
+};
--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { useState } from "react";
 import { StarIcon } from "@heroicons/react/solid";
 import Currency from "react-currency-formatter";
+import { useDispatch } from "react-redux";
+import { addToBasket } from "../slices/basketSlice";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -11,7 +13,23 @@ function Product({ id, title, price, description, category, image }) {
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
+  const dispatch = useDispatch();
+
   const [hasPrime] = useState(Math.random() < 0.5);
+
+  const addItemToBasket = () => {
+    const product = {
+      id,
+      title,
+      price,
+      description,
+      category,
+      image,
+      hasPrime,
+      rating,
+    };
+    dispatch(addToBasket(product));
+  };
 
   return (
     <div className="relative flex flex-col m-5 bg-white z-30 p-10">
@@ -42,7 +60,9 @@ function Product({ id, title, price, description, category, image }) {
         </div>
       )}
 
-      <button className="mt-auto button">Add to Basket</button>
+      <button onClick={addItemToBasket} className="mt-auto button">
+        Add to Basket
+      </button>
     </div>
   );
 }
