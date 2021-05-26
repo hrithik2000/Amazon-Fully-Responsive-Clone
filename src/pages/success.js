e9b8986dc87339012b11a81759d71f395e708048
hrithik <hrithik@PINGHERE.whitefoxx>
2021-05-26 12:50:39 -0400

    final

--- src/pages/success.js
+++ src/pages/success.js
@@ -4,10 +4,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import Header from "../components/Header";
 import { clearBasket } from "../slices/basketSlice";
-<<<<<<< HEAD
 import Footer from "../components/Footer";
-=======
->>>>>>> edc122ad62fa08fc7d11849dcb13b1c280f12200
 
 function success() {
   const router = useRouter();
@@ -41,10 +38,7 @@ function success() {
           </button>
         </div>
       </main>
-<<<<<<< HEAD
       <Footer />
-=======
->>>>>>> edc122ad62fa08fc7d11849dcb13b1c280f12200
     </div>
   );
 }
